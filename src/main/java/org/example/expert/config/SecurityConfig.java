258080922ad2a67c62e8bf5b39a79dbe2221e1a4
jubseok <junseok708@gmail.com>
2024-11-20 11:19:49 +0900

    fix : Lv 9  Security 적용, config 추가 설정

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -1,33 +1,67 @@
 package org.example.expert.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.expert.config.jwt.JwtAuthenticationFilter;
+import org.example.expert.config.jwt.JwtAuthorizationFilter;
+import org.example.expert.config.jwt.JwtUtil;
+import org.example.expert.config.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrf)->csrf.disable())
+                .csrf((csrf) -> csrf.disable())
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin((auth)->auth.disable());
+                .formLogin((auth) -> auth.disable());
 
-        http.authorizeRequests((authorizeRequests)->{
+        http.authorizeRequests((authorizeRequests) -> {
             authorizeRequests
-                    .requestMatchers("/auth/signup","/auth/signin").permitAll()
-                    .requestMatchers("/admin").hasRole("ADMIN")
+                    .requestMatchers("/auth/**", "/**").permitAll()
                     .anyRequest().authenticated();
         });
 
-        http.sessionManagement((session)->session
+        http.sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
