3252da4e0947adc500677ca8782aff0ab01fb540
jubseok <junseok708@gmail.com>
2024-11-20 11:21:07 +0900

    fix : Lv 9  Security 적용, 자잘한 수정

--- src/main/java/org/example/expert/config/PersistenceConfig.java
+++ src/main/java/org/example/expert/config/PersistenceConfig.java
@@ -1,12 +1,9 @@
 package org.example.expert.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class PersistenceConfig { }
+public class PersistenceConfig {
+}
